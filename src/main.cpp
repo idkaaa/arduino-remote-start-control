d4ddc3c63263277993697e2c8c24561b1dbb84c1
clh <chris.hiles@telscreen.com>
2025-07-02 10:05:54 -0700

    removed ota updater

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include <Arduino.h>
 #include "wifi_setup.h"
 #include "async_web_server.h"
-#include "ota_updater.h"
 
 void setup() {
   Serial.begin(115200);
   wifi_init();
   server_init();
-  ota_updater_init();
 }
 
 void loop() {
-  ota_updater_check();
 }
